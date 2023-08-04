1c5fcbc8a70a142b8a57dd69321036a1285c9007
Roger Chen <rogerchen803@gmail.com>
2023-08-04 14:56:25 -0700

    Modified statistics to show data from database

---
+++ src/Components/Heatmap.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useRef } from "react";
+import * as d3 from "d3";
+
+const Heatmap = ({ data }) => {
+  const ref = useRef();
+  const cellSize = window.innerHeight / 10;
+
+  useEffect(() => {
+    const svg = d3.select(ref.current);
+    const colorScale = d3.scaleSequential().interpolator(d3.interpolateBlues);
+    svg
+      .selectAll("rect")
+      .data(data)
+      .join("rect")
+      .attr("y", (d, i) => (i % 7) * cellSize)
+      .attr("x", (d, i) => Math.floor(i / 7) * cellSize)
+      .attr("width", cellSize - 1)
+      .attr("height", cellSize - 1)
+      .attr("fill", (d) => colorScale(d / 10));
+  }, [data, cellSize]);
+
+  const numWeeks = Math.ceil(data.length / 7);
+  const svgWidth = numWeeks * cellSize;
+  const svgHeight = 7 * cellSize;
+
+  return (
+    <div style={{ overflow: "auto", height: "100%" }}>
+      <svg ref={ref} width={svgWidth} height={svgHeight} />
+    </div>
+  );
+};
+
+export default Heatmap;
--- src/Pages/Statistics.js
+++ src/Pages/Statistics.js
@@ -1,35 +1,29 @@
-import React, { useEffect, useRef } from "react";
-import * as d3 from "d3";
+import Heatmap from "../Components/Heatmap";
+import { useSelector } from "react-redux";
 
 function Statistics() {
-  const data = Array.from({ length: 365 }, () =>
-    Math.floor(Math.random() * 10)
-  );
+  const user = useSelector((state) => state.rigor);
 
-  const ref = useRef();
-  const cellSize = 12;
+  // const data = Array.from({ length: 365 }, () =>
+  //   Math.floor(Math.random() * 10)
+  // );
 
-  useEffect(() => {
-    const svg = d3.select(ref.current);
-    const colorScale = d3.scaleSequential().interpolator(d3.interpolateBlues);
-    svg
-      .selectAll("rect")
-      .data(data)
-      .join("rect")
-      .attr("y", (d, i) => (i % 7) * cellSize)
-      .attr("x", (d, i) => Math.floor(i / 7) * cellSize)
-      .attr("width", cellSize - 1)
-      .attr("height", cellSize - 1)
-      .attr("fill", (d) => colorScale(d / 10));
-  }, [data]);
-
-  const numWeeks = Math.ceil(data.length / 7);
-  const svgWidth = numWeeks * cellSize;
-  const svgHeight = 7 * cellSize;
+  function formatDate(date) {
+    const options = { year: "numeric", month: "long", day: "numeric" };
+    return date.toLocaleDateString(undefined, options);
+  }
+  console.log(user);
+  if (!user || user.length === 0) return <div>Is Loading</div>;
 
   return (
-    <div style={{ overflow: "auto", height: "100%" }}>
-      <svg ref={ref} width={svgWidth} height={svgHeight} />
+    <div>
+      {user.rigor.map((time) => (
+        <p key={time.date}>
+          {formatDate(new Date(time.date))} {time.minutesFocused}
+        </p>
+      ))}
+
+      {/* <Heatmap data={data} /> */}
     </div>
   );
 }
